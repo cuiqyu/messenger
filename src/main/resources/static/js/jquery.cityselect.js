abc41d395c90524f5b34d8ea52e4458486bb3355
Your Name <futing@unlifemedia.cn>
2020-06-01 16:34:14 +0800

    修改js

--- src/main/resources/static/js/jquery.cityselect.js
+++ src/main/resources/static/js/jquery.cityselect.js
@@ -21,7 +21,7 @@
 
         // 默认值
         settings = $.extend({
-            url: "js/city.min.js",
+            url: "../js/city.min.js",
             prov: null,
             city: null,
             dist: null,
